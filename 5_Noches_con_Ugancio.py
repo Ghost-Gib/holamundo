eaf9f9616827df434bb3deea7251147bf0c1679c
Ghost-Gib <elrandom989@gmail.com>
2025-05-29 22:57:29 -0400

    omagad

---
+++ 5_Noches_con_Ugancio.py
@@ -0,0 +1,69 @@
+# EJERCICIO 1
+
+# Ejecutar = str(input("Se ejecutara el lavamanos: "))
+# Agua = False
+
+# if Ejecutar == "Si":
+#     Agua = True
+#     print ("Se realizara la ejecución ahora mismo")
+#     print ("El agua esta cayendo")
+# elif Ejecutar == "No":
+#     Agua = False
+#     print ("Vuelva cuando quiera")
+# else:
+#     print ("Lavese las manos es un sucio")
+
+# EJERCICIO 2 
+
+# import time
+# Ejecutar = str(input("Desea prender la luz?: "))
+# Luz = False
+
+# if Ejecutar == "Si":
+#     Luz = True
+#     print ("Se hizo la luz")
+#     print ("Explota el techo")
+# elif Ejecutar == "No":
+#     Luz = False
+#     print ("Aparecera...")
+#     time.sleep(3)
+#     print ("FOXY!!!")
+#     time.sleep(1)
+#     print ("Mueres por un Jampscure")
+# else:
+#     print ("Aprenda a escribir mamaguevazo")
+
+# EJERCICIO 3
+
+
+# import time
+# Cafe = False
+# Person = str(input('''"Una Persona esta tomando el café:
+#     ("Esa persona te esta mirando un buen rato...")               
+#     ("Ryuk: Vaya, vaya, un humano tomando el café conm intenciones sospechosas hacia ti")
+#     Ryuk: Deseas usar este cuaderno? Escribe su nombre y veras que sucede...: '''))
+# time.sleep(2)
+# print ('''
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢶⣦⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠈⠹⡆⢀⣤⣤⡀⢠⣤⢠⣤⣿⡤⣴⡆⠀⣴⠀⠀⠀⢠⣄⠀⢠⡄⠀⠀⠀⣤⣄⣿⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠰⠆⠀⣷⢸⣧⣀⡀⢸⢹⡆⠀⢸⡇⠠⣧⢤⣿⠀⠀⠀⢸⡟⣦⣸⡇⡞⡙⢣⡀⢠⡇⠀⢿⠋⠛⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⣠⠟⢸⣇⣀⡀⣿⠉⢻⡀⢸⡇⠀⣿⠀⣿⠀⠀⠀⣸⡇⠘⢿⡏⢇⣁⡼⠃⣼⠃⠀⣼⡓⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#          ⠀⠀⠀⡿⠒⠋⠁⠀⠈⠉⠉⠁⠉⠀⠀⠀⠀⠉⠀⠉⠀⠉⠀⠀⠀⠉⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠛⠓⠲⠂⠀⠀''')
+
+# if Person == "si":
+#     Cafe = True
+#     print ("Ryuk: Ya esta tomada la decisión")
+#     time.sleep(2)
+#     print ("La persona que te estaba viendo era una chica por que te veia atractivo pero ahora por ponerle") 
+#     print (" su nombre en el cuaderno el café que tomaba le hizo hervir los organos hasta explotar y perdiste la oportunidad de ponerla")
+# elif Person == "no":
+#     Cafe = False 
+#     print ("Ryuk: Pense que fueras mas interesante")
+#     print ("Ryuk se va a llorar por su cuchurrumin (Kira)")
+# else:
+#     print ("Ponen tu nombre en la death note por no responder bien y pal lobby")
+
+# EJERCICIO 3
+a=0
+while a!=3:
+    a=int(input("Ingresa un numero: "))
---
+++ EVA_03_FRMTIVA.py
@@ -0,0 +1,6 @@
+Pikachu_Roll = 4500
+Otaku_Roll = 5000
+Pulpo_Venenoso_Roll = 5200
+Anguila_Eléctrica_Roll = 4800
+
+def
